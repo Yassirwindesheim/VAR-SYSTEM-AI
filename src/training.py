fa3434fc96477b46a91bb0a16a0626772ccd332b
Yassirwindesheim <yassirbady3@gmail.com>
2025-11-18 19:48:28 +0100

    Update training.py

--- src/training.py
+++ src/training.py
@@ -7,23 +7,21 @@ from pathlib import Path  # Importeer Path om paden correct te hanteren
 MODEL_NAAM = 'yolov8_attack_direction'
 
 # RELATIEF PAD naar de HOOFDMAP van de dataset.
-# We gebruiken Path() om het pad correct te construeren, ongeacht het OS (Windows/Linux)
+
 # Dit pad is RELATIEF t.o.v. de 'offside-ai-full' map.
 DATASET_ROOT_PATH = Path('../data/My First Project.v1-datasetfootball.folder') 
-# Aangezien je het script uitvoert vanuit de 'src' map, moeten we één map terug (..)
 
-# Zorg ervoor dat het pad nu een absolute string is voor YOLO
 YOLO_DATA_PATH = str(DATASET_ROOT_PATH.resolve())
 
-# 1. Controleer of het pad bestaat voor de zekerheid
+
 if not Path(YOLO_DATA_PATH).exists():
     raise FileNotFoundError(f"Fout: Dataset map niet gevonden op {YOLO_DATA_PATH}")
 
-# 2. Model laden (Classification)
+#  Model laden (Classification)
 print("Model laden: yolov8n-cls.pt")
 model = YOLO('yolov8n-cls.pt') 
 
-# 3. Training uitvoeren
+#  Training uitvoeren
 print("Training van Classification Model gestart...")
     
 results = model.train(
